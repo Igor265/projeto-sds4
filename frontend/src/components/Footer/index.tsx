6d58c2d71cd98dcd1f092349870d14b5f42f5483
Igor Ribeiro <IgorJFR@outlook.com>
2021-09-10 17:14:26 -0300

    Footer update

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/Igor265" target="_blank" rel="noreferrer">Igor Ribeiro</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/igor-ribeiro-a1a670174/" target="_blank" rel="noreferrer">Igor Ribeiro</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
                     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
